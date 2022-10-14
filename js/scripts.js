08dc8d6a3e54c41bd04197cf64eabdec1f5e9178
Sherpad Ndabambi <sgndabambi@gmail.com>
2022-10-14 02:13:00 +0200

    Added fallback data

--- js/scripts.js
+++ js/scripts.js
@@ -191,105 +191,418 @@ function displayTicketsPerAgentChart(ticketsPerAgent) {
 
 function getAgentRatings() {
     return new Promise((resolve) => {
-        $.get("php/getAgentRatings.php", (result) => {
 
-            // declare local variables
-            let agents = [],
-                avg_ratings = [],
-                max_ratings = [],
-                min_ratings = [],
+        // declare local variables
+        let agentRatings,
+            agents = [],
+            avg_ratings = [],
+            max_ratings = [],
+            min_ratings = [];
+        try {
+            $.get("php/getAgentRatings.php", (result) => {
                 agentRatings = JSON.parse(result);
-
-            agentRatings.forEach((row) => {
-                agents.push(row.agent);
-                avg_ratings.push(parseFloat(row.avg_rating));
-                max_ratings.push(parseFloat(row.max_rating));
-                min_ratings.push(parseFloat(row.min_rating));
             });
+        }
+        catch(error) {
+            console.log(error);
+            agentRatings = [
+                {
+                    agent: "Nick Fury",
+                    min_rating: 3,
+                    avg_rating: 4.341463414634147,
+                    max_rating: 5
+                },
+                {
+                    agent: "Natasha Romanoff",
+                    min_rating: 3,
+                    avg_rating: 4.351851851851852,
+                    max_rating: 5
+                },
+                {
+                    agent: "Melinda May",
+                    min_rating: 3,
+                    avg_rating: 4.402985074626866,
+                    max_rating: 5
+                },
+                {
+                    agent: "Leo Fitz",
+                    min_rating: 1,
+                    avg_rating: 4.133333333333334,
+                    max_rating: 5
+                },
+                {
+                    agent: "Jemma Simmons",
+                    min_rating: 2,
+                    avg_rating: 3.842857142857143,
+                    max_rating: 5
+                },
+                {
+                    agent: "Daisy Johnson",
+                    min_rating: 3,
+                    avg_rating: 4.148351648351649,
+                    max_rating: 5
+                },
+                {
+                    agent: 'Antoinne "Trip" Triplett',
+                    min_rating: 3,
+                    avg_rating: 4,
+                    max_rating: 5
+                },
+                {
+                    agent: "Lance Hunter",
+                    min_rating: 3,
+                    avg_rating: 4.264285714285714,
+                    max_rating: 5
+                },
+                {
+                    agent: "Lincoln Campbell",
+                    min_rating: 3,
+                    avg_rating: 4.244186046511628,
+                    max_rating: 5
+                },
+                {
+                    agent: 'Elena "Yo-Yo" Rodriguez',
+                    min_rating: 3,
+                    avg_rating: 4.383561643835616,
+                    max_rating: 5
+                }
+            ];
+        }
 
-            resolve({
-                agents: agents,
-                avg_ratings: avg_ratings,
-                max_ratings: max_ratings,
-                min_ratings: min_ratings
-            });
+        agentRatings.forEach((row) => {
+            agents.push(row.agent);
+            avg_ratings.push(parseFloat(row.avg_rating));
+            max_ratings.push(parseFloat(row.max_rating));
+            min_ratings.push(parseFloat(row.min_rating));
+        });
+
+        resolve({
+            agents: agents,
+            avg_ratings: avg_ratings,
+            max_ratings: max_ratings,
+            min_ratings: min_ratings
         });
     });
 }
 
 function getAgentsPerTicket() {
     return new Promise((resolve) => {
-        $.get("php/getAgentsPerTicket.php", (result) => {
 
-            // declare local variables
-            let agentsPerTicket = JSON.parse(result),
-                output = [];
+        // declare local variables
+        let agentsPerTicket,
+        output = [];
 
-            agentsPerTicket.forEach((row) => {
-                output.push({
-                    name: row.number_of_agents,
-                    y: parseFloat(row.number_of_tickets)
-                });
+        try {
+            $.get("php/getAgentsPerTicket.php", (result) => {
+                agentsPerTicket = JSON.parse(result);
             });
-            resolve(output);
+        }
+        catch(error) {
+            console.log(error);
+            agentsPerTicket = [
+                {
+                    number_of_agents: 1,
+                    number_of_tickets: 107
+                },
+                {
+                    number_of_agents: 2,
+                    number_of_tickets: 74
+                }, 
+                {
+                    number_of_agents: 3,
+                    number_of_tickets: 196
+                }
+            ];
+        }
+
+        agentsPerTicket.forEach((row) => {
+            output.push({
+                name: row.number_of_agents,
+                y: parseFloat(row.number_of_tickets)
+            });
         });
+
+        resolve(output);
     });
 }
 
 function getResolutionTime() {
     return new Promise((resolve) => {
-        $.get("php/getResolutionTime.php", (result) => {
-            resolve(JSON.parse(result));
-        });
+        try {
+            $.get("php/getResolutionTime.php", (result) => {
+                resolve(JSON.parse(result));
+            });
+        }
+        catch(error) {
+            console.log(error);
+            resolve({
+                avg_resolution_time: 238.06969045,
+                max_resolution_time: 620.8939,
+                min_resolution_time: 1.9275
+            });
+        }
     });
 }
 
 function getResolutionTimePerMonth() {
     return new Promise((resolve) => {
-        $.get("php/getResolutionTimePerMonth.php", (result) => {
 
-            // declare local variables
-            let avg_resolution_times = [],
-                max_resolution_times = [],
-                min_resolution_times = [],
-                month_names = [],
+        // declare local variables
+        let avg_resolution_times = [],
+            max_resolution_times = [],
+            min_resolution_times = [],
+            month_names = [],
+            resolutionTimesPerMonth;
+
+        try {
+            $.get("php/getResolutionTimePerMonth.php", (result) => {
                 resolutionTimesPerMonth = JSON.parse(result);
-
-            resolutionTimesPerMonth.forEach((row) => {
-                avg_resolution_times.push(parseFloat(row.avg_resolution_time));
-                max_resolution_times.push(parseFloat(row.max_resolution_time));
-                min_resolution_times.push(parseFloat(row.min_resolution_time));
-                month_names.push(row.month_name);
             });
-
-            resolve({
-                avg_resolution_times: avg_resolution_times,
-                max_resolution_times: max_resolution_times,
-                min_resolution_times: min_resolution_times,
-                months: month_names
-            });
+        }
+        catch(error) {
+            console.log(error);
+            resolutionTimesPerMonth = [
+                {
+                    avg_resolution_time: 187.05890000,
+                    max_resolution_time: 490.1572,
+                    min_resolution_time: 32.5833,
+                    month_name: "Jan"
+                },
+                {
+                    avg_resolution_time: 243.75820000,
+                    max_resolution_time: 575.6756,
+                    min_resolution_time: 2.8372,
+                    month_name: "Feb"
+                },
+                {
+                    avg_resolution_time: 208.15490000,
+                    max_resolution_time: 539.4922,
+                    min_resolution_time: 1.9275,
+                    month_name: "Mar"
+                },
+                {
+                    avg_resolution_time: 277.65732308,
+                    max_resolution_time: 538.0008,
+                    min_resolution_time: 6.0742,
+                    month_name: "Apr"
+                },
+                {
+                    avg_resolution_time: 187.54847027,
+                    max_resolution_time: 453.8578,
+                    min_resolution_time: 18.4197,
+                    month_name: "May"
+                },
+                {
+                    avg_resolution_time: 279.48512619,
+                    max_resolution_time: 585.0625,
+                    min_resolution_time: 23.8600,
+                    month_name: "Jun"
+                },
+                {
+                    avg_resolution_time: 214.57615854,
+                    max_resolution_time: 620.8939,
+                    min_resolution_time: 15.3358,
+                    month_name: "Jul"
+                },
+                {
+                    avg_resolution_time: 282.42937083,
+                    max_resolution_time: 595.4883,
+                    min_resolution_time: 6.1817,
+                    month_name: "Aug"
+                },
+                {
+                    avg_resolution_time: 234.12002800,
+                    max_resolution_time: 538.6525,
+                    min_resolution_time: 20.4778,
+                    month_name: "Sep"
+                },
+                {
+                    avg_resolution_time: 302.08278333,
+                    max_resolution_time: 572.1792,
+                    min_resolution_time: 128.8636,
+                    month_name: "Oct"
+                }
+            ];
+        }
+        
+        resolutionTimesPerMonth.forEach((row) => {
+            avg_resolution_times.push(parseFloat(row.avg_resolution_time));
+            max_resolution_times.push(parseFloat(row.max_resolution_time));
+            min_resolution_times.push(parseFloat(row.min_resolution_time));
+            month_names.push(row.month_name);
+        });
+        
+        resolve({
+            avg_resolution_times: avg_resolution_times,
+            max_resolution_times: max_resolution_times,
+            min_resolution_times: min_resolution_times,
+            months: month_names
         });
     });
 }
 
 function getTicketsPerAgent() {
     return new Promise((resolve) => {
-        $.get("php/getTicketsPerAgent.php", (result) => {
 
-            // declare local variables
-            let agents = [],
-                ticketsPerAgent = JSON.parse(result),
-                number_of_tickets = [];
+        // declare local variables
+        let agents = [],
+            ticketsPerAgent,
+            number_of_tickets = [];
 
-            ticketsPerAgent.forEach((row) => {
-                agents.push(row.agent);
-                number_of_tickets.push(parseInt(row.number_of_tickets));
+        try {
+            $.get("php/getTicketsPerAgent.php", (result) => {
+                ticketsPerAgent = JSON.parse(result);
             });
+        }
+        catch(error) {
+            console.log(error);
+            ticketsPerAgent = [
+                {
+                    agent: 'Peggy Carter',
+                    number_of_tickets: 24
+                },
+                {
+                    agent: 'Howard Stark',
+                    number_of_tickets: 27
+                },
+                {
+                    agent: 'Daniel Sousa',
+                    number_of_tickets: 26
+                },
+                {
+                    agent: 'Hank Pym',
+                    number_of_tickets: 20
+                },
+                {
+                    agent: 'Janet Van Dyne',
+                    number_of_tickets: 20
+                },
+                {
+                    agent: 'Nick Fury',
+                    number_of_tickets: 19
+                },
+                {
+                    agent: 'Clint Barton',
+                    number_of_tickets: 24
+                },
+                {
+                    agent: 'Natasha Romanoff',
+                    number_of_tickets: 31
+                },
+                {
+                    agent: 'Phil Coulson',
+                    number_of_tickets: 30
+                },
+                {
+                    agent: 'Melinda May',
+                    number_of_tickets: 26
+                },
+                {
+                    agent: 'Leo Fitz',
+                    number_of_tickets: 11
+                },
+                {
+                    agent: 'Jemma Simmons',
+                    number_of_tickets: 29
+                },
+                {
+                    agent: 'Grant Ward',
+                    number_of_tickets: 28
+                },
+                {
+                    agent: 'Daisy Johnson',
+                    number_of_tickets: 30
+                },
+                {
+                    agent: 'Maria Hill',
+                    number_of_tickets: 30
+                },
+                {
+                    agent: 'John Garett',
+                    number_of_tickets: 27
+                },
+                {
+                    agent: 'Victoria Hand',
+                    number_of_tickets: 18
+                },
+                {
+                    agent: 'Eric Koenig',
+                    number_of_tickets: 30
+                },
+                {
+                    agent: 'Mike Peterson',
+                    number_of_tickets: 23
+                },
+                {
+                    agent: 'Antoinne "Trip" Triplett',
+                    number_of_tickets: 18
+                },
+                {
+                    agent: 'Alphonso "Mack" Mackenzie',
+                    number_of_tickets: 28
+                },
+                {
+                    agent: 'Lance Hunter',
+                    number_of_tickets: 35
+                },
+                {
+                    agent: 'Bobbi Morse',
+                    number_of_tickets: 32
+                },
+                {
+                    agent: 'Lincoln Campbell',
+                    number_of_tickets: 27
+                },
+                {
+                    agent: 'Holden Radcliffe',
+                    number_of_tickets: 30
+                },
+                {
+                    agent: 'Elena "Yo-Yo" Rodriguez',
+                    number_of_tickets: 18
+                },
+                {
+                    agent: 'Deke Shaw',
+                    number_of_tickets: 21
+                },
+                {
+                    agent: 'Jefferey Mace',
+                    number_of_tickets: 32
+                },
+                {
+                    agent: 'Billy Koenig',
+                    number_of_tickets: 17
+                },
+                {
+                    agent: 'Sam Koenig',
+                    number_of_tickets: 23
+                },
+                {
+                    agent: 'LT Koenig',
+                    number_of_tickets: 17
+                },
+                {
+                    agent: 'Thurston Koenig',
+                    number_of_tickets: 25
+                },
+                {
+                    agent: 'Ernest Hazard Koenig',
+                    number_of_tickets: 20
+                },
+                {
+                    agent: 'Arnim Zola',
+                    number_of_tickets: 27
+                }
+            ];
+        }
+        ticketsPerAgent.forEach((row) => {
+            agents.push(row.agent);
+            number_of_tickets.push(parseInt(row.number_of_tickets));
+        });
 
-            resolve({
-                agents: agents,
-                number_of_tickets: number_of_tickets
-            });
+        resolve({
+            agents: agents,
+            number_of_tickets: number_of_tickets
         });
     });
 }
